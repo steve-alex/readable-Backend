464c977e9afafeb194ce2675fab700c89ef57c04
steve-alex <steve.alex@hotmail.co.uk>
2019-12-10 18:40:17 +0000

    created followers and following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ApplicationRecord
-  # has_secure_password
   has_many :shelves
   has_many :shelfBooks, through: :shelves
   has_many :likes
   has_many :comments
   has_many :reviews
   has_many :progresses
+
+  # has_many :relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  # has_many :followed_users, through: :relationships, source: :followed
+
+  # has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  # has_many :followers, through: :reverse_relationships, source: :follower
+
+  has_many :follows_as_follower, foreign_key: "follower_id", class_name: :Follow, dependent: :destroy
+  has_many :following, through: :follows_as_follower, class_name: :User
+  
+  has_many :follows_as_followed, foreign_key: "followed_id", class_name: :Follow, dependent: :destroy
+  has_many :followers, through: :follows_as_followed, class_name: :User
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_092542) do
+ActiveRecord::Schema.define(version: 2019_12_10_183233) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -19,10 +19,21 @@ ActiveRecord::Schema.define(version: 2019_12_10_092542) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.string "content"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -33,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_092542) do
   end
 
   create_table "progesses", force: :cascade do |t|
+    t.integer "status"
     t.integer "user_id", null: false
     t.integer "shelfBook_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -42,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_092542) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.string "content"
     t.integer "user_id", null: false
     t.integer "shelfBook_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -68,8 +81,14 @@ ActiveRecord::Schema.define(version: 2019_12_10_092542) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "fullname"
+    t.boolean "fullnameviewable"
     t.string "username"
     t.string "password_digest"
+    t.string "gender"
+    t.string "city"
+    t.boolean "cityviewable"
+    t.string "about"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
