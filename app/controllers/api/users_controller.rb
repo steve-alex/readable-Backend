9e66c82412a8cce06f9c6838c89a6151814e75e7
steve-alex <steve.alex@hotmail.co.uk>
2019-12-10 15:25:56 +0000

    created user authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,20 @@
 class Api::UsersController < ApplicationController
     def create
+        user = User.create(user_params)
+        byebug
+        if user.valid?
+            render json: {user: UserSerializer.new(user), token: issue_token({ user_id: user.id })}
+        else
+            render json: user.errors.full_messages, status: :not_accepted
+        end
+    end
 
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_092542) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
