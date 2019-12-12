2bce00bc6985dc659564d287bdb2864f3313ae9a
steve-alex <steve.alex@hotmail.co.uk>
2019-12-12 13:50:43 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do 
-    resources :users, only: [:create, :show]
+    resources :books, only: [:create, :show, :update, :destroy]
+    resources :comments, only: [:create, :show, :destroy]
+    resources :follows, only: [:create, :show, :destroy]
+    resources :likes, only: [:create, :show, :destroy]
+    resources :progresses, only: [:create, :show, :update, :destroy]
+    resources :reviews, only: [:create, :show, :update, :destroy]
+    resources :shelf_books, only: [:create, :show, :update, :destroy]
+    resources :shelves, only: [:create, :show, :update, :destroy]
+    resources :users, only: [:create, :show, :update, :destroy]
   end
 
 end
