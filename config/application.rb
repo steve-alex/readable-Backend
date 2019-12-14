b8b54d310172c3d8dd0443e9cf71e7447a9065b5
steve-alex <steve.alex@hotmail.co.uk>
2019-12-14 18:36:02 +0000

    Allowed CORS

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,13 @@ module ReadableBackend
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
     
   end
 end
