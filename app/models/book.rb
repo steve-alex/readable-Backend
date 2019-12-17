a1bcb3afc67cb0a41910ed378bce4f520dee3ca7
steve-alex <steve.alex@hotmail.co.uk>
2019-12-17 22:21:10 +0000

    Book model methods created

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,7 @@
 class Book < ApplicationRecord
   has_many :shelf_books
   has_many :shelves, through: :shelf_books
-
-  def reviews
-    self.shelf_books.map{ |shelf_book| shelf_book.reviews }.flatten
-  end
+  has_many :reviews
 
   def progresses
     self.shelf_books.map{ |shelf_book| shelf_book.progresses }.flatten
@@ -12,7 +9,7 @@ class Book < ApplicationRecord
 
   def most_appeared_on_shelves
     shelf_count_hash = {}
-    shelf_names = self.shelf_books.map{ |shelf_book| shelf_book.shelf.name.downcase! }
+    shelf_names = self.shelves.map{ |shelf| shelf.name.downcase! }
     shelf_names.each { |shelf_name| 
       if shelf_count_hash[shelf_name]
         shelf_count_hash[shelf_name] += 1
@@ -26,4 +23,28 @@ class Book < ApplicationRecord
   def currently_reading_count
     self.shelf_books.count{ |shelf_book| shelf_book.currently_reading == true }
   end
+
+  def current_users_reviews(current_user)
+    self.reviews.select{ |review| review.user_id == 1 }
+  end
+
+  def followed_users_reviews(current_user)
+    followed_ids = current_user.followed.map{ |user| user.id }
+    self.reviews.select{ |review| followed_ids.include?(review.user_id)}
+  end
+
+  def review_distribution
+    review_distribution = {"review_count" => 0}
+    self.reviews.each{ |review|
+      rating = review.rating
+      review_distribution["review_count"] += 1
+      if review_distribution[rating]
+        review_distribution[rating] += 1
+      else
+        review_distribution[rating] = 1
+      end
+    }
+    review_distribution
+  end
+  
 end
