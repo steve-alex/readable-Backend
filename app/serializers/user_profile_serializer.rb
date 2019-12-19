8fb650c14859a0572f38ec32666e49cc189abdb0
steve-alex <steve.alex@hotmail.co.uk>
2019-12-19 17:32:41 +0000

    created User profile serializer

---
+++ app/serializers/user_profile_serializer.rb
@@ -0,0 +1,45 @@
+class UserProfileSerializer
+
+  def initialize(user, current_user)
+    @user = user
+    @current_user = current_user
+  end
+
+  def serialize_as_json
+    {
+      user: {
+        id: @user.id,
+        fullname: get_fullname(),
+        username: @user.username,
+        email: @user.email, 
+        gender: @user.gender, 
+        city: get_city(),
+        about: @user.about,
+        follower_count: @user.followers.length,
+        following_count: @user.followed.length,
+        book_count: @user.unique_books.length
+      },
+      currently_readng: nil,
+      shelves: @user.profile_shelf_display, 
+      genres: @user.genres,
+      favourite_genres: @user.favourite_genres,
+      favourite_authors: @user.favourite_authors,
+      books_in_common: @user.books_in_common(@current_user)
+    }
+  end
+
+  def get_fullname
+    if @user.fullnameviewable
+      return @user.fullname
+    end
+    nil
+  end
+
+  def get_city
+    if @user.cityviewable
+      return @user.city
+    end
+    nil
+  end
+
+end
