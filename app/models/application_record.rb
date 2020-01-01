08d7144f6ce043c69da4c2d5f004ef21f3350132
steve-alex <steve.alex@hotmail.co.uk>
2020-01-01 14:58:47 +0000

    Created time since upload function

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,35 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def time_since_upload
+    seconds_since_upload = Time.new - self.created_at
+    calculate_time_since_upload(seconds_since_upload)
+  end
+
+  def calculate_time_since_upload(time)
+    if (time % 60 == time)
+      if (time <= 20)
+        return "Just now"
+      else
+        return "#{time.round} seconds ago"
+      end
+    elsif (time % 3600 == time)
+      if (time == 1)
+        return "#{(time / 60).round} minute ago"
+      else
+        return "#{(time / 60).round} minutes ago"
+      end
+    elsif (time % 86400 == time)
+      if (time == 1)
+        return "#{(time / 3600).round} hour ago" 
+      else
+        return "#{(time / 3600).round} hours ago" 
+      end
+    elsif (time % 604800 == time)
+      return "#{(time / 86400).round} days ago"
+    else
+      return "#{(time / 604800).round} weeks ago"
+    end
+  end
+
 end
